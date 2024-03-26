14dda958b4606e70aa644d52f373e1571673fa6e
shivammakwan <shivammakwan124@gmail.com>
2024-03-27 03:31:32 +0530

    live

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    version: "1.0.0",
+    userMessage:
+      "It worked",
+    status: 200,
+    fareManagerId: "X91SMSS",
+  });
+});
+
+app.get("/:fail", (req, res) => {
+  console.log(req.get("host"));
+  res.status(200).send({
+    version: "1.0.0",
+    userMessage:
+      "Sorry, Something happened unexpectedly. Please try after sometime.",
+    status: 404,
+    fareManagerId: "X91SMSS",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
